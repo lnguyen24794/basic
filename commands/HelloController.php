18e505aaf142d50e022ecf29966683a2001d3e98
Tuan Nguyen <nguyen_thanhtuan@dev.co-mit.com>
2016-07-05 11:54:02 +0700

    fix

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -9,14 +9,7 @@ namespace app\commands;
 
 use yii\console\Controller;
 
-/**
- * This command echoes the first argument that you have entered.
- *
- * This command is provided as an example for you to learn how to create console commands.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
+
 class HelloController extends Controller
 {
     /**
